ad247f1129a498f101feab8de96b7de1fb449a76
belser-sada <ben.elser@sada.com>
2020-11-19 00:04:37 -0600

    updated businesslinefactory scaffolding

--- modules/businesslinefactory/variables.tf
+++ modules/businesslinefactory/variables.tf
@@ -6,24 +6,3 @@ variable "business_line_object" {
         projects = list(string)
     })
 }
-
-# variable name {
-#     type = string
-#     description = "Name of the business line"
-# }
-
-# variable enviornments {
-#   type        = list
-#   description = "The name of the GCP folders that make up each environment"
-#   default     = ["dev", "qa", "prod", "rnd"]
-# }
-
-# variable business_line_products {
-#     type = list(string)
-#     description = "All child products of a business line"
-# }
-
-# variable business_line_projects {
-#     type = list(string)
-#     description = "Projects that belong to parent business line and product"
-# }
