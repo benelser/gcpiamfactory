d764262855d602d4c48694183fc647b69761c504
= <=>
2020-12-04 12:27:56 -0600

    updated readme

--- main.tf
+++ main.tf
@@ -7,8 +7,9 @@ locals {
   businessunit_iam_definitions = fileset("${path.module}/businessunits", "*")
 }
 
-module "bizunits" {
+module "bizunit" {
   for_each    = toset(local.businessunit_iam_definitions[*])
+  # In production this source needs to reference a version of businessunitiamfactory source control URI
   source     = "./modules/businessunitiamfactory"
   org_domain = var.org_domain
   business_unit = jsondecode(file("${path.module}/businessunits/${each.key}"))
